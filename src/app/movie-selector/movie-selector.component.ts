8764cb3210821fee45072862aa1c8a4bfd78a1f3
ahoareau <ahoareau.external@overkiz.com>
2024-04-19 15:12:56 +0200

    ✨ (movie): create movie selector

---
+++ src/app/movie-selector/movie-selector.component.ts
@@ -0,0 +1,28 @@
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { Genre } from '../service/models/genre.model';
+import { TmdbService } from '../service/tmdb.service';
+
+@Component({
+    selector: 'app-movie-selector',
+    standalone: true,
+    imports: [],
+    templateUrl: './movie-selector.component.html',
+    styleUrl: './movie-selector.component.scss'
+})
+export class MovieSelectorComponent implements OnInit {
+    tmdbService = inject(TmdbService);
+    genres: Genre[] | undefined;
+
+    constructor() {
+        effect(() => {
+            const genresResponse = this.tmdbService.fetchGenres();
+            if (genresResponse) {
+                this.genres = genresResponse.genres;
+            }
+        });
+    }
+
+    ngOnInit() {
+        this.tmdbService.fetchGenres();
+    }
+}
--- src/app/service/tmdb.service.ts
+++ src/app/service/tmdb.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { computed, inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { GenresResponse } from './models/genre.model';
 import { MovieApiResponse, MovieImageSize } from './models/movie.model';
 
 @Injectable({
@@ -20,10 +21,23 @@ export class TmdbService {
     });
     fetchTrendMovies = computed(() => this._fetchTrendMovies());
 
+    private _fetchGenres: WritableSignal<GenresResponse> = signal({
+        genres: [],
+    });
+    fetchGenres = computed(() => this._fetchGenres());
+
     getTrendMovies(): void {
         this.http.get<MovieApiResponse>(this.baseUrl + '/trending/movie/day', { headers: this.headers })
             .subscribe({
                 next: tmdbResponse => this._fetchTrendMovies.set(tmdbResponse),
+                error: error => console.log(error),
+            });
+    }
+
+    getAllGenre(): void {
+        this.http.get<GenresResponse>(this.baseUrl + '/genre/movie/list', { headers: this.headers })
+            .subscribe({
+                next: genresResponse => this._fetchGenres.set(genresResponse),
                 error: error => console.log(error),
             });
     }
